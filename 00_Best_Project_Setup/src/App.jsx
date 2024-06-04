467ee56138e600719aabf6b03fb749328f0591af
A H Shobuj <ahshobuj09@gmail.com>
2024-06-04 14:37:53 +0600

    Best_project_setup

---
+++ 00_Best_Project_Setup/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h2>Projects Setup</h2>
+            <ul>
+                <li>components</li>
+            </ul>
+        </>
+    );
+}
+
+export default App;
